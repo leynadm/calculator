b6cbee753cc0f30788601d58490108dc5032cb22
Leynad <matei_daniel@outlook.com>
2022-07-03 01:59:46 +0200

    Add the framework for the calculator, mockup HTML and initial JS functions structure

---
+++ script.js
@@ -0,0 +1,19 @@
+function operate (firstNumber, secondNumber, operator){
+
+}
+
+function addition(a,b){
+
+}
+
+function subtraction(a,b){
+
+}
+
+function multiplication(a,b){
+
+}
+
+function division(a,b){
+    
+}
