2254b4a63f2b9e0b7534a1c74fe101c7eafd71a0
Leynad <matei_daniel@outlook.com>
2022-07-16 01:46:09 +0200

    Add version 1.0 of working table with CSS

--- script.js
+++ script.js
@@ -15,50 +15,54 @@ let previousOperator;
 let saveFirstNumber;
 let temporaryNumber;
 
-// Add event listener to the operator keys
+
+
+window.addEventListener('keydown', function(e){
+    const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+    key.click();
+});
+
 for (let i = 0; i < operatorKeys.length; i++){
     operatorKeys[i].addEventListener('click',recordOperatorPress,false)
 }
 
-// Add event listener to the number keys
 for (let i = 0; i < numberKeys.length; i++){
     numberKeys[i].addEventListener('click',recordNumberPress,false)
 }
 
-// Add unique event listener to the equal key
 btnEqual.addEventListener('click',recordEqualPress);
 
-// Add unique event listener to the clear key
 btnC.addEventListener('click',clearNumbers);
 
-// Add unique event listener to the fix key
 btnFix.addEventListener('click',undoError);
 
 function recordNumberPress(e){
-
-    let numberPressed = e.target.textContent;
+    
+    let numberPressed = e.currentTarget.textContent;
+    
     if (operatorChosen == null){   
-        if (checkDotStatus(numberPressed) == "ALREADY"){
-            return;
-        }
-        if (firstNumberAdded.length > 17){
-            return;
-        }
 
-        firstNumberAdded = firstNumberAdded + e.target.textContent;
+            if (checkDotStatus(numberPressed) == "ALREADY"){
+                return;
+            }
+            if (firstNumberAdded.length > 17){
+                return;
+            }
+
+        firstNumberAdded = firstNumberAdded + e.currentTarget.textContent;
         inputBar.textContent = firstNumberAdded;
-        console.log('first number: ' + firstNumberAdded);
 
     } else {
-        if (checkDotStatus(numberPressed) == "ALREADY"){
-            return;
-        }
-        if (secondNumberAdded.length > 17){
-            return;
-        }
-        secondNumberAdded = secondNumberAdded + e.target.textContent;
+
+            if (checkDotStatus(numberPressed) == "ALREADY"){
+                return;
+            }
+            if (secondNumberAdded.length > 17){
+                return;
+            }
+
+        secondNumberAdded = secondNumberAdded + e.currentTarget.textContent;
         inputBar.textContent = secondNumberAdded;
-        console.log('second number: ' + secondNumberAdded);
     }
 }
 
@@ -88,11 +92,10 @@ function recordOperatorPress(e){
         operatorChosen = '';
     }
     previousOperator = operatorChosen;
-    console.log("first number is: " + firstNumberAdded);
-    console.log("second number is: " + secondNumberAdded);
 }
 
 function displayResult(resultValue){
+
     if (isFinite(resultValue)){
         inputBar.textContent = Math.round(resultValue*10000000)/10000000;
     } else {
@@ -101,6 +104,7 @@ function displayResult(resultValue){
 }
 
 function recordEqualPress(e){
+
     if (firstNumberAdded != '' && secondNumberAdded != ''){
     inputBar.textContent = inputBar.textContent + e.target.textContent;
     let result = operate(firstNumberAdded,secondNumberAdded,operatorChosen);
@@ -133,7 +137,6 @@ function undoError(e){
             inputBar.textContent = inputBarTextString;
         }
     }
-    
 }
 
 function operate (firstNumber, secondNumber, operator){
@@ -171,6 +174,5 @@ function multiplication(a,b){
 
 function division(a,b){
     let result = parseFloat(a) / parseFloat(b);
-    console.log('division result: ' + result)
     return result;
 }
